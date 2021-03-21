45d6c8f62171c22e8a63d4551430bfabeaef5c58
maxwellrk <maxkatz23@gmail.com>
2021-03-20 21:34:45 -0400

    Added example get and post request to test file

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,8 +1,22 @@
-const request = require('supertest');
+const request = require('supertest')('https://jsonplaceholder.typicode.com');
 
 describe('API Tests', () => {
-    it('Makes a GET req', () => {});
-    it('Makes a POST req', () => {});
+    console.log('hello');
+    it('Makes a GET req', () => {
+        request
+            .get('/comments')
+            .expect(200)
+            .then((resp) => console.log(resp.status))
+            .catch((err) => console.log(err));
+    });
+    it('Makes a POST req', () => {
+        request
+            .post('/posts')
+            .send({ userId: 100, title: 'example title' })
+            .expect(201)
+            .then((resp) => console.log(resp.body))
+            .catch((err) => console.log(err));
+    });
     it('Makes a PUT req', () => {});
     it('Makes a DELETE req', () => {});
     it('Set Headers for request', () => {});
