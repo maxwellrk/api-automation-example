c0258af0afeef9a2fa9463e1562fd1b99e8d03c9
maxwellrk <maxkatz23@gmail.com>
2021-03-20 20:55:09 -0400

    Added api test it blocks

---
+++ tests/api.test.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+
+describe('API Tests', () => {
+    it('Makes a GET req', () => {});
+    it('Makes a POST req', () => {});
+    it('Makes a PUT req', () => {});
+    it('Makes a DELETE req', () => {});
+    it('Set Headers for request', () => {});
+    it('Set Body for the request', () => {});
+    it('Sucessfully returns 200 code', () => {});
+    it('Chains request to a new response', () => {});
+});
