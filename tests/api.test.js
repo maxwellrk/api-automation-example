3df72ef1c0fd8f4a97370b5f79f6112f8aeabb87
maxwellrk <maxkatz23@gmail.com>
2021-03-20 22:29:38 -0400

    Removed console.logs from tests. Added test for chained response to request

--- tests/api.test.js
+++ tests/api.test.js
@@ -3,33 +3,25 @@ const expect = require('chai').expect;
 
 describe('API Tests', () => {
     it('Makes a GET req', () => {
-        return request
-            .get('/comments')
-            .expect(200)
-            .then((resp) => console.log(resp.status));
+        return request.get('/comments').expect(200);
     });
 
     it('Makes a POST req', () => {
         return request
             .post('/posts')
             .send({ userId: 100, title: 'example title' })
-            .expect(201)
-            .then((resp) => console.log(resp.body));
+            .expect(201);
     });
 
     it('Makes a PUT req', () => {
         return request
             .put('/posts/1')
             .send({ userId: 100, title: 'example title' })
-            .expect(200)
-            .then((resp) => console.log(resp.body));
+            .expect(200);
     });
 
     it('Makes a DELETE req', () => {
-        return request
-            .delete('/posts/1')
-            .expect(200)
-            .then((resp) => console.log(resp.status));
+        return request.delete('/posts/1').expect(200);
     });
 
     it('Set Headers for request', () => {
@@ -38,16 +30,14 @@ describe('API Tests', () => {
             .set('Accept', 'application/json')
             .set('Cookie', '023u0jd0808108h1f')
             .send({ userId: 100, title: 'example title' })
-            .expect(200)
-            .then((resp) => console.log(resp.body));
+            .expect(200);
     });
 
     it('Set Body for the request', () => {
         return request
             .post('/posts')
             .send({ userId: 100, title: 'example title' })
-            .expect(201)
-            .then((resp) => console.log(resp.body));
+            .expect(201);
     });
 
     it('Sucessfully returns 200 code', () => {
@@ -56,5 +46,12 @@ describe('API Tests', () => {
             .then((resp) => expect(resp.status).to.equal(200));
     });
 
-    it('Chains request to a new response', () => {});
+    it('Chains request to a new response', () => {
+        return request
+            .get('/posts/1')
+            .expect(200)
+            .then((resp) => {
+                return request.post('/posts').send(resp.body).expect(201);
+            });
+    });
 });
